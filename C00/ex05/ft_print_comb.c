bf118feb7327e034a67bd1ff59d0ede990a3af78
JohnTrind <joaommtrindade@gmail.com>
2021-07-02 12:50:44 +0100

    ex 05

--- C00/ex05/ft_print_comb.c
+++ C00/ex05/ft_print_comb.c
@@ -7,34 +7,29 @@ void	ft_putchar(char c)
 
 void	ft_print_comb(void)
 {
-	int	numbers[3] = {48, 48, 48};
+	int	numbers[3] = {'0', '0', '0'};
 
-	while (numbers[0] <= 57)
+	while (numbers[0] <= '9')
 	{
 		if ((numbers[0] < numbers[1]) && (numbers[1] < numbers[2]))
 		{
 			ft_putchar(numbers[0]);
 			ft_putchar(numbers[1]);
 			ft_putchar(numbers[2]);
-			if (numbers[0] != 55){
+			if (numbers[0] != '7'){
 				ft_putchar(',');
 				ft_putchar(' ');
 			}
 		}
-		if (numbers[2]++ >= 57)
+		if (numbers[2]++ >= '9')
 		{
-			numbers[2] = 48;
+			numbers[2] = '0';
 			numbers[1]++;
 		}
-		if (numbers[1] == 58)
+		if (numbers[1] == ':' )
 		{
-			numbers[1] = 48;
+			numbers[1] = '0';
 			numbers[0]++;
 		}
 	}
 }
-int main(){
-	ft_print_comb();
-	return(0);
-}
-
